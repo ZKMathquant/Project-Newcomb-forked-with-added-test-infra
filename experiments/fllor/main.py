3f24df6f9c1aa9481e1099f14cfd5aa2346c80b8
Florian Lorkowski <florian@lorkow.ski>
2026-03-06 13:52:54 +0100

    experiments: test all environment/agent combinations

---
+++ experiments/fllor/main.py
@@ -0,0 +1,43 @@
+import argparse
+import numpy as np
+
+from ibrl.simulators import simulate
+from ibrl.utils import construct_environment,construct_agent
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="RL test with multi-armed bandit")
+    parser.add_argument("environment",          help="Environment type",            type=str)
+    parser.add_argument("agent",                help="Agent type",                  type=str)
+    parser.add_argument("-k", "--arms",         help="Number of arms",              type=int,       default=2)
+    parser.add_argument("-s", "--steps",        help="Number of steps per episode", type=int,       default=1001)
+    parser.add_argument("-r", "--runs",         help="Number of episodes to run",   type=int,       default=1)
+    parser.add_argument(      "--seed",         help="Random number seed",          type=int,       default=42)
+    parser.add_argument("-v", "--verbose",      help="Debug output",                action="count", default=0)
+    parsed_args = parser.parse_args()
+
+    options = {
+        "num_actions": parsed_args.arms,
+        "num_steps":   parsed_args.steps,
+        "num_runs":    parsed_args.runs,
+        "seed":        parsed_args.seed,
+        "verbose":     parsed_args.verbose
+    }
+    env = construct_environment(parsed_args.environment, options)
+    agent = construct_agent(parsed_args.agent, options)
+    results = simulate(env, agent, options)
+
+    # Print average reward obtained (for plotting)
+    average_reward = results["average_reward"]
+    optimal_reward = results["optimal_reward"]
+    for i in range(options["num_steps"]):
+        average_reward_spread = np.sqrt(average_reward[1,i] - average_reward[0,i]**2)
+        average_reward_unc = average_reward_spread / np.sqrt(options["num_runs"])
+
+        print(
+            i,
+            optimal_reward,          # Expected reward when using optimal policy (as determined by environment)
+            average_reward[0,i],     # Average reward received by agent
+            average_reward_unc,      # Uncertainty of reward (converges to 0 as num_runs goes to infinity)
+            average_reward_spread,   # Spread of reward (converges to constant)
+        )
---
+++ experiments/fllor/plot.gp
@@ -0,0 +1,38 @@
+ENV=ARG1
+AGENT=ARG2
+
+set style fill transparent solid 0.5
+
+set term pngcairo size 1024,512 linewidth 3 fontscale 1.5;
+set output "experiments/fllor/figures/".ENV.".".AGENT.".png";
+set grid;
+set key box bottom center opaque;
+
+set xlabel "Steps";
+set ylabel "Average reward";
+set title ((ENV eq "bandit")                 ? "Multi-armed bandit" : \
+           (ENV eq "newcomb")                ? "Newcomb's problem" : \
+           (ENV eq "damascus")               ? "Death in Damascus" : \
+           (ENV eq "asymmetric-damascus")    ? "Asymmetric Death in Damascus" : \
+           (ENV eq "coordination")           ? "Coordination game" : \
+           (ENV eq "pdbandit")               ? "Policy-dependent bandit" : \
+           (ENV eq "switching")              ? "Switching bandit" : \
+           ENV).", ". \
+          ((AGENT eq "classical")            ? "Q-learning agent" : \
+           (AGENT eq "bayesian")             ? "Bayesian agent" : \
+           (AGENT eq "exp3")                 ? "EXP3 agent" : \
+           (AGENT eq "experimental1")        ? "Experimental agent 1" : \
+           (AGENT eq "experimental2")        ? "Experimental agent 2" : \
+           AGENT);
+set yrange [0:];
+if(AGENT eq "experimental2" || AGENT eq "exp3") {
+     plot "experiments/fllor/outputs/".ENV.".".AGENT.".epsilon.txt" u 1:2 w l ls 1 title "Optimal policy", \
+          "experiments/fllor/outputs/".ENV.".".AGENT.".epsilon.txt" u 1:($3+$4):($3-$4) w filledcurves ls 2 notitle, \
+          "experiments/fllor/outputs/".ENV.".".AGENT.".epsilon.txt" u 1:3 w l ls 2 title "ε-greedy policy";
+} else {
+     plot "experiments/fllor/outputs/".ENV.".".AGENT.".epsilon.txt" u 1:2 w l ls 1 title "Optimal policy", \
+          "experiments/fllor/outputs/".ENV.".".AGENT.".epsilon.txt" u 1:($3+$4):($3-$4) w filledcurves ls 2 notitle, \
+          "experiments/fllor/outputs/".ENV.".".AGENT.".epsilon.txt" u 1:3 w l ls 2 title "ε-greedy policy", \
+          "experiments/fllor/outputs/".ENV.".".AGENT.".softmax.txt" u 1:($3+$4):($3-$4) w filledcurves ls 3 notitle, \
+          "experiments/fllor/outputs/".ENV.".".AGENT.".softmax.txt" u 1:3 w l ls 3 title "Softmax policy";
+}
